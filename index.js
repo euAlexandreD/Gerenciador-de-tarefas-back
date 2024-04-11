c5ca9b5f68449a471adda21b021cd5e6131db96b
Alexandre Portella <alexandreportella@Alexandres-MacBook-Pro.local>
2024-04-11 20:59:14 -0300

    feat: add route add task and save database

--- index.js
+++ index.js
@@ -7,12 +7,28 @@ const TaskModel = require("./src/model/task.model");
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 conectDataBase();
 
-app.get("/", async (req, res) => {
-    const tasks = await TaskModel.find({});
-    res.status(200).send(tasks);
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await TaskModel.find({});
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const newTasks = new TaskModel(req.body);
+        await newTasks.save();
+
+        res.status(201).send(newTasks);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 app.listen(8000, () => console.log("listening on port 8000"));
